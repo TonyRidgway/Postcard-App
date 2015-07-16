e146bbaedf3cd4e488ca3cacd549e1f47614d1a6
Tony Ridgway <tonyridgway51@Tonys-Macintosh.local>
2015-07-16 17:23:54 +0100

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameEntered: UILabel!
+    
+    
     @IBOutlet weak var myName: UITextField!
     @IBOutlet weak var myMessageTextField: UITextField!
+    
+    
     @IBOutlet weak var mailButton: UIButton!
     
 
@@ -33,6 +38,13 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = myMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameEntered.hidden = false
+        nameEntered.text = myName.text
+        nameEntered.textColor = UIColor.blueColor()
+        
+        myName.text = ""
+        myName.resignFirstResponder()
         
         
         myMessageTextField.text = ""
